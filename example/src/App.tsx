066cb45ce037e0d16c0181439f06e97837ac4703
Njoku Emmanuel <kalunjoku123@gmail.com>
2022-01-07 02:53:09 +0100

    chore: cleanup convention

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-lazerpay-react-native';
+import { multiply } from 'lazerpay-react-native';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
