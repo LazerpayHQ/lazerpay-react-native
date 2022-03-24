162be3b477c51abe25de2453cc2dd31d628989f0
franko4don <franko4don@yahoo.com>
2022-03-24 17:50:28 +0100

    fix clipboard issue for android

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,4 +1,6 @@
 const PAYMENT_SUCCESS = 'successful';
 const PAYMENT_ERROR = 'error';
 const PAYMENT_CLOSE = 'cancelled';
-export { PAYMENT_SUCCESS, PAYMENT_ERROR, PAYMENT_CLOSE };
+const COPIED = 'copied';
+const FETCHED = 'fetched';
+export { PAYMENT_SUCCESS, PAYMENT_ERROR, PAYMENT_CLOSE, COPIED, FETCHED };
